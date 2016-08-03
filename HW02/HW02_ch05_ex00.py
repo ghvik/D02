ddcbf43d149adf70e17617822ede11c83773d1ed
ghvik <vreddy@ischool.berkeley.edu>
2016-08-03 13:09:31 -0700

    Finished ch05 ex00, do_n

--- HW02/HW02_ch05_ex00.py
+++ HW02/HW02_ch05_ex00.py
@@ -8,13 +8,8 @@
 # Write your functions below:
 # Body
 
-
-
-
-
-
-
-
+def do_n(f, n):
+    [f() for x in range(n)]
 
 # Write your functions above:
 def print_hello():
@@ -25,8 +20,7 @@ def main():
     When complete have one function call in this function:
     do_n(print_hello, 10)
     """
-    print_hello() # replace this with do_n(print_hello, 10)
-
+    do_n(print_hello, 10) # replace this with do_n(print_hello, 10)
 
 
 if __name__ == "__main__":
